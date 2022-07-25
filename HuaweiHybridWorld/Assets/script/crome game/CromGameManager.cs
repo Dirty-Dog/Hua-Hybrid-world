143e4fcdd60f1ace7a0e7060b4ece2575a67fd7f
Shubham-Kumbhar <88823169+Shubham-Kumbhar@users.noreply.github.com>
2022-07-26 03:03:26 +0530

    did lot of stuf abt crom game
    11 baje se  kam kar raha hu bc + no of commit bhada ne ke lie parts me uplod kar raha hu haha har dhek ke maza atta hai

--- HuaweiHybridWorld/Assets/script/crome game/CromGameManager.cs
+++ HuaweiHybridWorld/Assets/script/crome game/CromGameManager.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CromGameManager : MonoBehaviour
 {
+    public int cromScore;
+    public GameObject gameOverCanvas;
+    private void Start()
+    {
+        cromScore = 0;
+        gameOverCanvas.SetActive(false);
+    }
     public void cromGameover()
     {
-        if(gameObject.CompareTag("enemy"))
         //pause game
-        Debug.Log("game over");
         Time.timeScale = 0;
+        //show canvas
+        gameOverCanvas.SetActive(true);
+    }
 
-        //show score 
+    public void cromScoreInc()
+    {
+        cromScore++;
+    }
+    
+    void restartCromGame()
+    {
+        SceneManager.LoadScene("Shubham scene crom game");
     }
 }
